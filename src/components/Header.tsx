86febc798815026ad8bf7e13adc5178855fa36ff
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-12 00:06:31 -0600

    Header finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
-import "98.css";
 import { Tabs } from "../interfaces/Tabs";
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
+import "98.css";
 
 export const Header = ({ tabs }: { tabs: Tabs[] }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -13,7 +9,7 @@ export const Header = ({ tabs }: { tabs: Tabs[] }) => {
   return (
     <header className="bg-white">
       <nav
-        className="mx-auto flex  items-center justify-center"
+        className="mx-auto flex items-center justify-center"
         aria-label="Global"
       >
         {/* Web menu */}
@@ -29,7 +25,7 @@ export const Header = ({ tabs }: { tabs: Tabs[] }) => {
               {tabs.map((tab) => (
                 <button>
                   {" "}
-                  <span> {tab.name} </span>{" "}
+                  <a href={tab.href}>{tab.name}</a>
                 </button>
               ))}
             </div>
@@ -44,30 +40,26 @@ export const Header = ({ tabs }: { tabs: Tabs[] }) => {
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-end">
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-red-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>X
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {tabs.map((tab, index) => (
-                  <a
-                    key={index}
-                    href={tab.href}
-                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {tab.name}
-                  </a>
-                ))}
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="window h-screen">
+            <div className="title-bar">
+              <div className="title-bar-text">Welcome to my Portfolio</div>
+              <div className="title-bar-controls">
+                <button
+                  aria-label="Close"
+                  onClick={() => setMobileMenuOpen(false)}
+                ></button>
               </div>
+            </div>
+            <div className="window-body m-4">
+              {tabs.map((tab, index) => (
+                <button
+                  key={index}
+                  className=" block my-2 py-2 px-3 text-base leading-7 w-1/2"
+                >
+                  <a href={tab.href}>{tab.name}</a>
+                </button>
+              ))}
             </div>
           </div>
         </Dialog.Panel>
