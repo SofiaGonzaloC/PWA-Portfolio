d2a837269c7b0f765a6ae805f1237cf8e181c8a1
SofiaGonzaloC <sofiagonzaloc@outlook.com>
2023-03-21 15:24:00 -0600

    Configuration set and images added, still not able to download

---
+++ src/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'v1_cache1_ASGC_PWA';
+
+var urlsToCache = [
+  '/index.html',
+  '/sw.js',
+  '/manifest.json',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache)
+        .then(() => { self.skipWaiting() })
+        .catch(err => console.log('Hubo un error', err))
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheNames) => {
+            if (cacheWhiteList.indexOf(cacheNames) == -1) {
+              return cache.delete(cacheNames);
+            }
+          })
+        );
+      })
+      .then(self.clients.claim())
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,45 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'generateSW', // 'generateSW' | 'injectManifest'
+      injectRegister: 'inline', // change to auto if fails
+      manifest: {
+        "theme_color": "#D986C1",
+        "background_color": "#1C192E",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "https://pwa-portfolio.vercel.app/",
+        "name": "SofiaGonzaloC",
+        "short_name": "SGC",
+        "description": "Sofia's portfolio",
+        "icons": [
+          {
+            "src": "images/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      },
+      workbox: {
+
+      },
+    })
   ]
 })
